e5cd5eeaab32f43ab6547641f141757134aa4f5f
MyGitHubBlueberry <MyGitHubBlueberry@gmail.com>
2026-06-10 13:48:12 +0200

    feat: work on auth service

--- Server/Services/AuthService.cs
+++ Server/Services/AuthService.cs
@@ -4,17 +4,38 @@ using Server.Core.Interfaces;
 using Server.Core.Models;
 using Server.Data;
 
+using System.IO;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using BCrypt.Net;
 
 public class AuthService(AppDbContext db) : IAuthService
 {
-    Task<string?> IAuthService.LoginAsync(string username, string password)
+    public async Task<string?> LoginAsync(string username, string password)
     {
-        throw new System.NotImplementedException();
+        var user = await db.Users.FirstOrDefaultAsync(u => u.Username == username)
+            ?? throw new InvalidDataException("Invalid login: No user with this username exists.");
+
+        if (!BCrypt.Verify(password, user.Password))
+            throw new InvalidDataException("Invalid login: Password is invalid.");
+
+        return "token"; //todo return jwt?;
     }
 
-    Task<User> IAuthService.RegisterAsync(string username, string password)
+    public async Task<User> RegisterAsync(string username, string password)
     {
-        throw new System.NotImplementedException();
+        if (await db.Users.AnyAsync(u => u.Username == username))
+            throw new InvalidDataException("Registration failed: This username is already in use.");
+
+        var pwd = BCrypt.HashPassword(password, BCrypt.GenerateSalt());
+        var user = new User
+        {
+            Username = username,
+            Password = pwd
+        };
+
+        await db.Users.AddAsync(user);
+        await db.SaveChangesAsync();
+        return user;
     }
 }
