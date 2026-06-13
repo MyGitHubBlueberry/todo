ef71ab7d93f388d8b6c4f0bd6611f2db10b6db47
MyGitHubBlueberry <MyGitHubBlueberry@gmail.com>
2026-06-13 12:19:11 +0200

    feat: started work on the front

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { AuthPage } from '@pages/auth-page';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: AuthPage,
+    title: 'Authentication Page'
+  }
+];
---
+++ src/pages/auth-page.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: 'auth-page',
+  templateUrl: './auth-page.html',
+  // styleUrl: './auth-page.css',
+  imports: [],
+})
+export class AuthPage { }
