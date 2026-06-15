6dac3c41cd466b934237c1c4a9127fdf7a0c8f9e
MyGitHubBlueberry <MyGitHubBlueberry@gmail.com>
2026-06-15 17:17:50 +0200

    chore: changed style of one dto naming conventions

--- Server/Core/DTOs/Task/TaskResponseDto.cs
+++ Server/Core/DTOs/Task/TaskResponseDto.cs
@@ -6,12 +6,12 @@ using System;
 using Server.Core;
 
 public record TaskResponseDto(
-    int Id,
-    string Title,
-    string? Body,
-    Status Status,
-    DateTime CreatedAt,
-    DateTime? UpdatedAt,
-    bool IsEdited,
-    CategoryResponseDto[] Categories
+    int id,
+    string title,
+    string? body,
+    Status status,
+    DateTime createdAt,
+    DateTime? updatedAt,
+    bool isEdited,
+    CategoryResponseDto[] categories
 );
