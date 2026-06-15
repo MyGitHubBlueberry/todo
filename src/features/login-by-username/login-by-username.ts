fd3381cc7073b746f8895f2d0bf8acf9f5646a68
MyGitHubBlueberry <MyGitHubBlueberry@gmail.com>
2026-06-15 13:58:51 +0200

    feat: login works

--- src/features/login-by-username/login-by-username.ts
+++ src/features/login-by-username/login-by-username.ts
@@ -1,5 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { ReactiveFormsModule, FormGroup, FormControl, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { LoginDto } from '@entities/session/api/types';
+import { SessionService } from '@entities/session/model/session.service';
 import { AuthFormLayoutComponent } from '@shared/ui/auth-form-layout/auth-form-layout';
 
 @Component({
@@ -8,6 +11,12 @@ import { AuthFormLayoutComponent } from '@shared/ui/auth-form-layout/auth-form-l
   imports: [ReactiveFormsModule, AuthFormLayoutComponent],
 })
 export class LoginByUsername {
+  private readonly router = inject(Router);
+  private readonly sessionService = inject(SessionService);
+  private readonly isLoading = signal(false);
+
+  protected readonly backendError = signal<string | null>(null);
+
   protected readonly loginForm = new FormGroup({
     login: new FormControl('', {
       nonNullable: true,
@@ -17,6 +26,24 @@ export class LoginByUsername {
   });
 
   protected login(): void {
-    alert('Attempted login with: ' + JSON.stringify(this.loginForm.value));
+    if (this.loginForm.invalid) return;
+
+    this.isLoading.set(true);
+    this.backendError.set(null);
+
+    var payload: LoginDto = {
+      login: this.loginForm.value.login!,
+      password: this.loginForm.value.password!,
+    };
+    this.sessionService.login(payload).subscribe({
+      next: _ => {
+        this.isLoading.set(false);
+        this.router.navigate(['/']);
+      },
+      error: err => {
+        this.isLoading.set(false);
+        this.backendError.set(err.error?.message || 'An unexpected error occurred.');
+      }
+    });
   }
 }
