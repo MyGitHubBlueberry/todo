043f897f908e8d8069268bf1eb5f946981b9bd4a
MyGitHubBlueberry <MyGitHubBlueberry@gmail.com>
2026-06-17 12:28:27 +0200

    feat: task-edit component

---
+++ src/features/task-edit/task-edit.component.ts
@@ -0,0 +1,31 @@
+import { Component, input, model, output, OnInit } from "@angular/core";
+import { FormsModule } from "@angular/forms";
+import { CategoryResponseDto } from "@entities/category/api/types";
+import { TaskResponseDto, TaskUpdateDto } from "@entities/task/api/types";
+import { TaskCategoriesComponent } from "@features/task-categories/task-categories.component";
+
+@Component({
+  selector: 'app-task-edit',
+  templateUrl: './task-edit.html',
+  imports: [FormsModule, TaskCategoriesComponent]
+})
+export class TaskEditComponent {
+  public readonly task = input.required<TaskResponseDto>();
+  public readonly categories = input.required<CategoryResponseDto[]>();
+
+  public readonly onSave = output<TaskUpdateDto>();
+  public readonly onClose = output<void>();
+
+  protected updatedTask!: TaskUpdateDto;
+
+  ngOnInit() {
+    const currentTask = this.task();
+    this.updatedTask = {
+      title: currentTask.title,
+      body: currentTask.body,
+      categoryIds: currentTask.categories
+        ? [...currentTask.categories.map(c => c.id)]
+        : [],
+    };
+  }
+}
