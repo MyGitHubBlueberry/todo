63429990fb4a9c5c05804402ea5e362e4be91eb2
MyGitHubBlueberry <MyGitHubBlueberry@gmail.com>
2026-06-21 14:47:38 +0200

    fix: showMore/less works in all modes

--- README.md
+++ README.md
@@ -6,6 +6,6 @@
 - [x] close only corresponding windows
 - [x] show errors from the server
 - [x] make sure site is fully on the screen
+- [x] fix show more/less
 - [ ] adapt for mobile
-- [ ] fix show more/less
 - [ ] cleanup git repo
--- src/entities/task/ui/task-card.ts
+++ src/entities/task/ui/task-card.ts
@@ -1,5 +1,14 @@
 import { DatePipe } from '@angular/common';
-import { Component, input, output, signal } from '@angular/core';
+import {
+  Component,
+  input,
+  output,
+  signal,
+  viewChild,
+  ElementRef,
+  AfterViewInit,
+  OnDestroy
+} from '@angular/core';
 import { TaskResponseDto } from '@entities/task/api/types';
 
 @Component({
@@ -7,11 +16,40 @@ import { TaskResponseDto } from '@entities/task/api/types';
   templateUrl: './task-card.html',
   imports: [DatePipe],
 })
-export class TaskCardComponent {
+export class TaskCardComponent implements AfterViewInit, OnDestroy {
   public task = input.required<TaskResponseDto>();
   public onToggle = output<TaskResponseDto>();
   public onEdit = output<TaskResponseDto>();
+  public textContainer = viewChild<ElementRef<HTMLParagraphElement>>('taskBody');
+
   protected readonly isExpanded = signal(false);
+  protected readonly isTextOverflowing = signal(false);
+
+  private resizeObserver?: ResizeObserver;
+
+  ngAfterViewInit() {
+    const element = this.textContainer()?.nativeElement;
+    if (!element) return;
+
+    this.resizeObserver = new ResizeObserver(() => {
+      this.checkOverflow(element);
+    });
+
+    this.resizeObserver.observe(element);
+
+    setTimeout(() => this.checkOverflow(element), 0);
+  }
+
+  ngOnDestroy() {
+    this.resizeObserver?.disconnect();
+  }
+
+  private checkOverflow(element: HTMLElement) {
+    if (!this.isExpanded()) {
+      const hasOverflow = element.scrollHeight > (element.clientHeight + 2);  // +2 helps it work on all browsers
+      this.isTextOverflowing.set(hasOverflow);
+    }
+  }
 
   toggleExpand() {
     this.isExpanded.update(val => !val);
